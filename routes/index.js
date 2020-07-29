c721b57e97c907bd329b2f81bcee15cfb68c4f1b
Thao Phung <thaophuong10498@gmail.com>
2020-07-29 14:16:22 -0500

    change waterfall to async/await syntax

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,20 @@ const express = require("express"),
   router = express.Router({ mergeParams: true }),
   passport = require("passport"),
   async = require("async"),
+
   nodemailer = require("nodemailer"),
+
+  // require sendgrid/mail
+  sgMail = require('@sendgrid/mail'),
+
   crypto = require("crypto"),
   User = require("../models/user"),
   StockSearch = require("../models/stocksearch"),
   got = require("got"),
   Stock = require("../models/stock");
 require("dotenv").config();
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 router.get("/test", async (req, res) => {
   const stocks = await StockNyse.find({});
@@ -132,61 +139,70 @@ router.get("/forgot", (req, res) => {
   res.render("forgot");
 });
 
-// handling forgot password logic
-router.post('/forgot', function(req, res, next) {
-  async.waterfall([
-    function(done) {
-      crypto.randomBytes(20, function(err, buf) {
-        var token = buf.toString('hex');
-        done(err, token);
-      });
-    },
-    function(token, done) {
-      User.findOne({ email: req.body.email }, function(err, user) {
-        if (!user) {
-          req.flash('error', 'No account with that email address exists.');
-          return res.redirect('/forgot');
-        }
+// forgot functions. crypo.randombytes does not support promises by default
+// so we have to "promisify" it.
+var generateResetToken = () => {
+	return new Promise((resolve, reject) => {
+		// crypto random bytes has a callback.
+		// randombytes(size[, callback])
+		crypto.randomBytes(20, (err, buf) => {
+			if (err) reject(err);
+			else {
+				let reset_token = buf.toString('hex');
+				resolve(reset_token);
+			}
+		})
+	})
+}
 
-        user.resetPasswordToken = token;
-        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+// NEW forgot post
+router.post('/forgot', async (req, res) => {
+	try {
+		// generate reset token to send.
+		let reset_token = await generateResetToken();
+		console.log(reset_token);
 
-        user.save(function(err) {
-          done(err, token, user);
-        });
-      });
-    },
-    function(token, user, done) {
-      var smtpTransport = nodemailer.createTransport({
-        service: 'Gmail', 
-        auth: {
-          user: 'stockoverflow.stockapp@gmail.com',
-          pass: process.env.GMAIL_PW
-        }
-      });
-      var mailOptions = {
-        to: user.email,
-        from: 'stockoverflow.stockapp@gmail.com', // email that users can reply to
-        subject: 'STOCKOVERFLOW Password Reset',
-        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-          'http://' + req.headers.host + '/reset/' + token + '\n\n' +
-          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-          // host: website host
-          // token: for server to verify
-      };
-      smtpTransport.sendMail(mailOptions, function(err) {
-        console.log('mail sent');
-        req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
-        done(err, 'done');
-      });
-    }
-  ], function(err) {
-    if (err) return next(err);
-    res.redirect('/forgot');
-  });
+		// find the specified user by email.
+		let user = await User.findOne({email: req.body.email});
+		if (!user) {
+      req.flash('error', 'No account with that email address.');
+			throw 'user not found.'
+		}
+		user.resetPasswordToken = reset_token;
+		user.resetPasswordExpires = Date.now() + 3600000; // 1 hour in ms
+		// passport local mongoose allows for promises inherently.
+		await user.save();
+
+    // create transport
+		var smtpTransport = nodemailer.createTransport({
+			service: 'Gmail',
+			auth: {
+				user: 'stockoverflow.stockapp@gmail.com',
+				pass: process.env.GMAIL_PW
+			}
+		});
+		// set options
+		var mailOptions = {
+			to: user.email,
+			from: 'stockoverflow.stockapp@gmail.com',
+			subject: 'StackOverflow Password Reset',
+			text: 'You are receiving this because you (or someone else) have requested the reset of the password linked to your Yelpcamp account.' +
+				'Please click on the following link, or paste this into your browser to complete the process.' + '\n\n' +
+				'http://' + req.headers.host + '/users/reset/' + reset_token + '\n\n' + 
+				'If you did not request this, please ignore this email and your password will remain unchanged.'
+		};
+		// send mail uses promise if no callback is specified.
+		await	smtpTransport.sendMail(mailOptions);
+		console.log('mail sent');
+		req.flash('success', 'an email has been sent to ' + user.email + ' with further instructions.');
+		res.redirect('/forgot');
+	} catch (error) {
+		console.log(error);
+		res.redirect('/forgot');
+	}	
 });
 
+// show reset form
 router.get('/reset/:token', function(req, res) {
   User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
     if (!user) {
@@ -197,56 +213,49 @@ router.get('/reset/:token', function(req, res) {
   });
 });
 
-router.post('/reset/:token', function(req, res) {
-  async.waterfall([
-    // waterfall: array of functions that gets called in sequence
-    function(done) {
-      User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
-        // $gt => greater than date
-        if (!user) {
-          req.flash('error', 'Password reset token is invalid or has expired.');
-          return res.redirect('back');
-        }
-        if(req.body.password === req.body.confirm) {
-          user.setPassword(req.body.password, function(err) {
-            user.resetPasswordToken = undefined;
-            user.resetPasswordExpires = undefined;
-
-            user.save(function(err) {
-              req.logIn(user, function(err) {
-                done(err, user);
-              });
-            });
-          })
-        } else {
-            req.flash("error", "Passwords do not match.");
-            return res.redirect('back');
-        }
-      });
-    },
-    function(user, done) {
-      var smtpTransport = nodemailer.createTransport({
-        service: 'Gmail', 
-        auth: {
-          user: 'stockoverflow.stockapp@gmail.com',
-          pass: process.env.GMAIL_PW
-        }
-      });
-      var mailOptions = {
-        to: user.email,
-        from: 'stockoverflow.stockapp@gmail.com',
-        subject: 'Your password has been changed',
-        text: 'Hello,\n\n' +
-          'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
-      };
-      smtpTransport.sendMail(mailOptions, function(err) {
-        req.flash('success', 'Success! Your password has been changed.');
-        done(err);
-      });
+// handling reset logic
+router.post('/reset/:token', async function(req, res) {
+  try {
+    let user = await User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now()}});
+    if (!user) {
+      req.flash('error', 'Password reset token is invalid or has expired.');
+      return res.redirect('back');
     }
-  ], function(err) {
-    res.redirect('/');
-  });
+    if(req.body.password === req.body.confirm) {
+      await user.setPassword(req.body.password);
+      user.resetPasswordToken = undefined;
+      user.resetPasswordExpires = undefined;
+      await user.save();
+      req.login();
+    } else {
+      req.flash("error", "Passwords do not match.");
+      return res.redirect('back');
+    }
+
+    var smtpTransport = nodemailer.createTransport({
+      service: 'Gmail', 
+      auth: {
+        user: 'stockoverflow.stockapp@gmail.com',
+        pass: process.env.GMAIL_PW
+      }
+    });
+    var mailOptions = {
+      to: user.email,
+      from: 'stockoverflow.stockapp@gmail.com',
+      subject: 'Your password has been changed',
+      text: 'Hello,\n\n' +
+        'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
+    };
+
+    // send mail uses promise if no callback is specified.
+		await	smtpTransport.sendMail(mailOptions);
+    console.log('mail sent');
+    req.flash('success', 'Success! Your password has been changed.');
+		res.redirect('/');
+  } catch (error) {
+		console.log(error);
+		res.redirect('/');
+  }	
 });
 
 router.get("*", function (req, res) {
