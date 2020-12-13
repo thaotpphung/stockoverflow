d810db38e6227c6dd75a6ed50122728a16b16320
Thao Phung <ttp7428@truman.edu>
2020-12-13 13:30:37 -0600

    Revert "changed transaction model to contain transaction information of 1 stock in 1 document"
    This reverts commit 923859cd604f0603dea39ef409ec1737cdb412c3.

--- controller/transaction.js
+++ controller/transaction.js
@@ -4,9 +4,14 @@ const User = require("../models/user"),
 
 exports.getTransactions = async (req, res) => {
   try {
-    let transactions = await Transaction.find({userid: req.params.userid});
+    let allTransactions = await Transaction.find({userid: req.params.userid});
+    // console.log("all transaciton", allTransactions);
+    let assets =  allTransactions.filter(transaction => transaction.isNewest == true);
+    console.log("assets", assets);
+    // eval(require("locus"));
+
     res.render("transactions/index", {
-      transactions: transactions,
+      transactions: allTransactions,
     });
   } catch(err) {
     console.log(err);
@@ -17,11 +22,12 @@ exports.getTransactions = async (req, res) => {
 exports.showNewTransactionFormByStock = async (req, res) => {
   try {
     let stock = await Stock.findOne({symbol: req.params.stocksymbol});
-    let transaction = await Transaction.findOne({symbol: req.params.stocksymbol});
-    if (transaction == null) {
+    let transactions = await Transaction.find({symbol: req.params.stocksymbol});
+    console.log('newest ', transactions[transactions.length - 1]);
+    if (transactions.length === 0) {
       res.render("transactions/new", { stock: stock, transaction: {totalquantity: 0} });
     } else {
-      res.render("transactions/new", { stock: stock, transaction: transaction });
+      res.render("transactions/new", { stock: stock, transaction: transactions[transactions.length - 1] });
     }
   } catch (err) {
     console.log(err);
@@ -36,39 +42,33 @@ exports.showNewTransactionForm = (req, res) => {
 
 exports.postTransaction = async (req, res) => {
   try {
-    let transaction = await Transaction.findOne(
-      {
-        symbol: req.body.transaction.symbol, 
-        userid : req.body.transaction.userid
-      }
-    );
-    if (!transaction) {  // if transaction of this stock has not been made b4
-      let transaction =  await Transaction.create(req.body.transaction); // create new transaction
-      await updateTransaction(transaction, req.body.transaction);
-    } else {
-      await updateTransaction(transaction, req.body.transaction);
+    let oldTransactions = await Transaction.find({symbol: req.body.transaction.symbol});
+    let transaction = await Transaction.create(req.body.transaction);
+    if (oldTransactions.length === 0) { // if transaction of this stock already exists
+      updateTotalByType (transaction, transaction, req.body.transaction, true);
+    } else {  // not exist before 
+      console.log("exists");
+      updateTotalByType (transaction, oldTransactions[oldTransactions.length - 1], req.body.transaction, false);
     }
     req.flash("success", "Successfully added transaction");
     res.redirect("/users/" + req.body.transaction.userid + "/transactions");
   } catch (err) {
     console.log(err);
-    req.flash("error", "Error occured, please try again later");
-    res.redirect("back");
   }
 }
 
-async function updateTransaction(transaction, transactionReq) {
-  let {price, time, quantity, type} = transactionReq;
-  price = Math.round(price * 100);
-  let newHistoryEntry = {price, time, quantity, type};
-  console.log('new', newHistoryEntry);
-  transaction.history.push(newHistoryEntry); 
-  if (type === "Purchase") {
-    transaction.totalprice += (Math.round(price * quantity)) ;
-    transaction.totalquantity += parseInt(quantity);
+async function updateTotalByType(transaction, oldTransaction, transactionReq, isFirst) {
+  if (transactionReq.type === "Purchase") {
+    transaction.totalprice = oldTransaction.totalprice + (Math.round(transactionReq.price * 100 * transactionReq.quantity)) ;
+    transaction.totalquantity = oldTransaction.totalquantity +  parseInt(transactionReq.quantity);
   } else {
-    transaction.totalprice -= (Math.round(price * quantity));
-    transaction.totalquantity -= parseInt(quantity);
+    transaction.totalprice = oldTransaction.totalprice - (Math.round(transactionReq.price * 100 * transactionReq.quantity));
+    transaction.totalquantity = oldTransaction.totalquantity - parseInt(transactionReq.quantity);
+  }
+  if (!isFirst) {
+    oldTransaction.isNewest = false;
+    transaction.isNewest = true;
+    oldTransaction.save();
   }
   transaction.save();
   return;
--- models/transaction.js
+++ models/transaction.js
@@ -4,16 +4,22 @@ var transactionSchema = mongoose.Schema({
   userid: String,
   symbol: String,
   name: String,
-  history: [  // transaction history of this stock
-    {
-      price: Number,
-      time: String,
-      quantity: Number,
-      transactiontype: String
-    }
-  ],
+  // stockid: String,
+  // history: [  // transaction history of this stock
+  //   {
+  //     price: Number,
+  //     time: String,
+  //     quantity: Number,
+  //     transactiontype: String
+  //   }
+  // ],
+  price: Number,
+  time: String,
+  quantity: Number,
+  transactiontype: String,
   totalprice: {type: Number, default: 0},
   totalquantity: {type: Number, default: 0},
+  isNewest: {type: Boolean, default: true },
 });
 
 transactionSchema.statics.getAllByUserId = function (userid, cb) {
